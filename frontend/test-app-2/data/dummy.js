67916e171ab4986db9617819dc9a364aba110800
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2025-02-15 18:25:57 +0400

    Removed everything

--- frontend/test-app-2/data/dummy.js
+++
@@ -1,8 +0,0 @@
-export const DUMMY_DATA = [
-
-    {id: 1, title: 'title 1', description: 'description 1'},
-    {id: 2, title: 'title 2', description: 'description 2'},
-    {id: 3, title: 'title 3', description: 'description 3'}
-
-
-]
