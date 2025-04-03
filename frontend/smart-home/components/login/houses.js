351c5a30e1558ca246a2c311316061a74958ca30
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2025-04-03 11:15:26 +0400

    One minor bug fix putting in a missing /

--- frontend/smart-home/components/login/houses.js
+++ frontend/smart-home/components/login/houses.js
@@ -51,7 +51,7 @@ export default function Houses() {
             ? "http://127.0.0.1:8000/api/landlords/"
             : "http://10.0.2.2:8000/api/landlords/";
             */
-        const apiUrl = `${API_BASE_URL}/api/landlords`;
+        const apiUrl = `${API_BASE_URL}/api/landlords/`;
 
         try {
             const response = await fetch(apiUrl, {
